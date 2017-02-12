3b33cf0ac55d5256f30b92edb02035fb8eb7ae0e
Joseph Atkins-Turkish <jat1g11@soton.ac.uk>
2017-02-12 14:08:10 -0800

    Added new expense form

--- client/src/components/ExpensesTable.js
+++ client/src/components/ExpensesTable.js
@@ -2,29 +2,54 @@ import React, { Component } from 'react';
 import _ from 'lodash'
 import 'css/ExpensesTable.css';
 
+function ExpenseEntry() {
+    return (<tr> 
+        <td>
+            <input type="date" name="date" />
+            <input type="time" name="time" />
+        </td>
+        <td><input type="number" step="0.01" min="0" name="amount" placeholder="0.00" /></td>
+        <td><input type="text" name="description" placeholder="Description of expense" /></td>
+        <td><input type="submit" /></td>
+    </tr>)
+}
+
 function ExpenseRow({amount, description, datetime, id}) {
-	return (<tr key={id}>
-		<td>{datetime}</td><td>{amount}</td><td>{description}</td>
-	</tr>)
+    return (<tr>
+        <td>{datetime}</td>
+        <td>{amount}</td>
+        <td>{description}</td>
+        <td>Edit</td>
+    </tr>)
 }
 
 class ExpensesTable extends Component {
-  render() {
-  	const {expenses} = this.props
-    return (
-      <div className="ExpensesTable">
-        <h3>Your expenses</h3>
-        <table>
-	        <thead>
-	        	<tr><th>Date</th><th>Amount</th><th>Description</th></tr>
-	        </thead>
-	        <tbody>
-	        	{_.map(expenses, (e)=><ExpenseRow {...e} />)}
-	        </tbody>
-        </table>
-      </div>
-    );
-  }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.createExpense(e)        
+    }
+
+    render() {
+        const {expenses} = this.props
+        return (
+          <div className="ExpensesTable">
+            <h3>Your expenses</h3>
+            <form onSubmit={this.handleSubmit}>
+            <table>
+                <thead>
+                    <tr><th>Date</th><th>Amount</th><th>Description</th><th></th></tr>
+                </thead>
+                <tbody>
+                    {_.map(expenses, (e)=><ExpenseRow key={e.id} {...e} />)}
+                </tbody>
+                <tfoot>
+                    <ExpenseEntry />
+                </tfoot>
+            </table>
+            </form>
+          </div>
+        );
+    }
 }
 
 export default ExpensesTable;
--- client/src/containers/Expenses.js
+++ client/src/containers/Expenses.js
@@ -5,12 +5,13 @@ class Expenses extends Component {
   render() {
     var fake_expenses = [
       {id: 1, amount: '$15.00', datetime: '1992-12-12'},
-      {id: 2, amount: '$45.00', datetime: '1992-12-15'},
+      {id: 2, amount: '$45.00', datetime: '1992-12-15', description: 'KFC blowout'},
       {id: 3, amount: '$125.00', datetime: '1992-12-21'},
       {id: 4, amount: '$125.00', datetime: '1992-12-24'},
       ];
+    var createExpense = (e)=>console.log("Created expense", e)
     return (
-      <ExpensesTable expenses={fake_expenses} />
+      <ExpensesTable expenses={fake_expenses} createExpense={createExpense} />
     );
   }
 }
